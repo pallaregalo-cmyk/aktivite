f8171abce227757da9f35a0baae0552ce007e2a4
pallaregalo-cmyk <pallaregalo@gmail.com>
2026-04-09 02:25:58 +0300

    Update server.js

--- server.js
+++ server.js
@@ -5,10 +5,6 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname)));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
-});
-
 app.listen(PORT, '0.0.0.0', () => {
   console.log('Sunucu çalışıyor: ' + PORT);
 });
